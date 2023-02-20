4a342cb52fa136a0b5d60867275bb96256842b39
wang1hm <1700011625@pku.edu.cn>
2023-02-20 22:03:05 +0800

    Update CaliFlux.py

--- code/CaliFlux.py
+++ code/CaliFlux.py
@@ -36,38 +36,41 @@ fig, ax = plt.subplots(2,1)
 
 Emission_name = ['Ha','Hb','MgII','[OIII]',' ','[NII]',' ','[SII]',' ','CIII','CIV','Lya']
 #Emission_wave = [6563.,4861.,2798,4959,5007,6548,6584,6716,6731,1909,1549,1216]*(1.+redshift[i])
-
-SDSS_path = './Huimei/Filter/SDSS/'
-mag_sdss_g = pd.read_csv(SDSS_path+'g.dat',names=['Del','lam_g','RC_g'],sep=' ')
-mag_sdss_g = mag_sdss_g.drop(labels = 'Del',axis=1)
-mag_sdss_r = pd.read_csv(SDSS_path+'r.dat',names=['Del','lam_g','RC_g'],sep=' ')
-mag_sdss_r = mag_sdss_r.drop(labels = 'Del',axis=1)
-mag_sdss_i = pd.read_csv(SDSS_path+'i.dat',names=['Del','lam_g','RC_g'],sep=' ')
-mag_sdss_i = mag_sdss_i.drop(labels = 'Del',axis=1)
-mag_sdss_z = pd.read_csv(SDSS_path+'z.dat',names=['Del','lam_g','RC_g'],sep=' ')
-mag_sdss_z = mag_sdss_z.drop(labels = 'Del',axis=1)
-mag_sdss_g['lam_g']=mag_sdss_g['lam_g']*10.
-mag_sdss_r['lam_g']=mag_sdss_r['lam_g']*10.
-mag_sdss_i['lam_g']=mag_sdss_i['lam_g']*10.
-mag_sdss_z['lam_g']=mag_sdss_z['lam_g']*10.
-
-PSTR_path = './Huimei/Filter/Panstarr/'
-mag_pstr_g = pd.read_csv(PSTR_path+'g.dat',names=['Del','lam_g','RC_g'],sep=' ')
-mag_pstr_g = mag_pstr_g.drop(labels = 'Del',axis=1)
-mag_pstr_r = pd.read_csv(PSTR_path+'r.dat',names=['Del','lam_g','RC_g'],sep=' ')
-mag_pstr_r = mag_pstr_r.drop(labels = 'Del',axis=1)
-mag_pstr_i = pd.read_csv(PSTR_path+'i.dat',names=['Del','lam_g','RC_g'],sep=' ')
-mag_pstr_i = mag_pstr_i.drop(labels = 'Del',axis=1)
-mag_pstr_z = pd.read_csv(PSTR_path+'z.dat',names=['Del','lam_g','RC_g'],sep=' ')
-mag_pstr_z = mag_pstr_z.drop(labels = 'Del',axis=1)
-mag_pstr_g['lam_g']=mag_pstr_g['lam_g']*10.
-mag_pstr_r['lam_g']=mag_pstr_r['lam_g']*10.
-mag_pstr_i['lam_g']=mag_pstr_i['lam_g']*10.
-mag_pstr_z['lam_g']=mag_pstr_z['lam_g']*10.
+def read_mag(photometry):
+    if photometry == 'SDSS':
+        SDSS_path = './Huimei/Filter/SDSS/'
+        mag_sdss_g = pd.read_csv(SDSS_path+'g.dat',names=['Del','lam_g','RC_g'],sep=' ')
+        lam_g = mag_sdss_g['lam_g']
+        mag_sdss_r = pd.read_csv(SDSS_path+'r.dat',names=['Del','lam_r','RC_r'],sep=' ')
+        lam_r = mag_sdss_r['lam_r']
+        mag_sdss_i = pd.read_csv(SDSS_path+'i.dat',names=['Del','lam_i','RC_i'],sep=' ')
+        lam_i = mag_sdss_i['lam_i']
+        mag_sdss_z = pd.read_csv(SDSS_path+'z.dat',names=['Del','lam_z','RC_z'],sep=' ')
+        lam_z = mag_sdss_z['lam_z']
+        lam_g=lam_g*10.
+        lam_r=lam_r*10.
+        lam_i=lam_i*10.
+        lam_z=lam_z*10.
+    if photometry == 'Panstarr':
+        PSTR_path = './Huimei/Filter/Panstarr/'
+        mag_pstr_g = pd.read_csv(PSTR_path+'g.dat',names=['Del','lam_g','RC_g'],sep=' ')
+        lam_g = mag_ptsr_g['lam_g']
+        mag_pstr_r = pd.read_csv(PSTR_path+'r.dat',names=['Del','lam_r','RC_r'],sep=' ')
+        lam_r = mag_ptsr_r['lam_r']
+        mag_pstr_i = pd.read_csv(PSTR_path+'i.dat',names=['Del','lam_i','RC_i'],sep=' ')
+        lam_i = mag_ptsr_i['lam_i']
+        mag_pstr_z = pd.read_csv(PSTR_path+'z.dat',names=['Del','lam_z','RC_z'],sep=' ')
+        lam_z = mag_ptsr_z['lam_z']
+        lam_g=lam_g*10.
+        lam_r=lam_r*10.
+        lam_i=lam_i*10.
+        lam_z=lam_z*10.
+    return lam_g, lam_r, lam_i, lam_z
 
 def RedBlue(p,z,photometry,name):
     Emission_wave = np.array([6563.,4861.,2798,4959,5007,6548,6584,6716,6731,1909,1549,1216])*(1.+z)
     specdata = fits.open('Volumes/My_Passport/Calibration/lamost_dr45/'+name+'.fits.gz')
+    lam_g, lam_r, lam_i, lam_z = read_mag(photometry)
     flux = specdata[0].data[2]#1e-17
     ivar = specdata[0].data[1]
     lam = specdata[0].data[2]
@@ -82,5 +85,10 @@ def RedBlue(p,z,photometry,name):
     flux_min     = min([flux1,flux2])
     lam_min=int(min(lam))+1
     lam_max=int(max(lam))
-    lam_bin=indgen(lam_max-lam_min)+lam_min
+    lam_bin=np.arange(0,lam_max-lam_min,1)+lam_min
+    #set the bin for griz band
+    lam_bin_g = lam_bin[np.where((min(lam_g)<lam_bin)&(lam_bin < max(lam_g)))]
+    lam_bin_r = lam_bin[np.where((min(lam_r)<lam_bin)&(lam_bin < max(lam_r)))]
+    lam_bin_i = lam_bin[np.where((min(lam_i)<lam_bin)&(lam_bin < max(lam_i)))]
+    lam_bin_z = lam_bin[np.where((min(lam_z)<lam_bin)&(lam_bin < max(lam_z)))]
 
